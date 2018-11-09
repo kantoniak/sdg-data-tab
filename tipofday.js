958248df0c569050124e5478fd1b3a7a77ad1045
Łukasz Kędziorski <kedziorski.lukasz@gmail.com>
2018-11-09 22:37:35 +0100

    Add descriptions to tip of the days

--- tipofday.js
+++ tipofday.js
@@ -1,12 +1,28 @@
-function createTipOfTheDay(indicator, areaCode) {
+function getData(indicator, areaCode) {
+    if (localStorage.getItem(indicator) === null) {
+        return axios.get('https://unstats.un.org/SDGAPI/v1/sdg/Indicator/Data?indicator=' + indicator + '&areaCode=' + areaCode)
+        .then(function (response) {
+            //console.log(response);
+            localStorage.setItem(indicator, JSON.stringify(response));
+        });
+    }
+}
+
+function createTipOfTheDay(indicator, desc) {
     item = JSON.parse(localStorage.getItem(indicator));
     if (item.data.data.length > 1) {
         message = item.data.data[0];
         message2 = item.data.data[item.data.data.length-1];
-        console.log(message);
-        console.log(message2);
-        console.log('Alkohol urus o ' + (parseFloat(message2.value) - parseFloat(message.value)) + ' od roku ' + message.timePeriodStart + ' do roku ' + message2.timePeriodStart)
+        numericalData = [(parseFloat(message2.value) - parseFloat(message.value)) , message.timePeriodStart, message2.timePeriodStart];
+        res = "";
+        desc.forEach(function (descPart, idx) { res += descPart + numericalData[idx]; });
+        console.log(res);
     }
 }
 
-createTipOfTheDay("3.5.2", "616")
+description = [['Alkohol urus o ', ' od roku ', ' do roku '], ['aaa ', ' bbb ', ' ccc '], ['ddd ', ' eee ', ' fff ']];
+indicators = ['3.5.2', '3.1.1', '4.1.1'];
+PolskaJestNajwazniejsza = '616';
+
+indicators.forEach(function (indicator) { getData(indicator, PolskaJestNajwazniejsza); });
+indicators.forEach(function (indicator, idx) { createTipOfTheDay(indicator, description[idx]); });
