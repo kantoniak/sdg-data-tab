5ad2c583b8696333db267d9b66f5b980a20d4010
Krzysztof Antoniak <krzysztof@antoniak.in>
2018-11-10 16:01:37 +0100

    Fix sharing links, add tab navigation

--- main-footer.js
+++ main-footer.js
@@ -1,3 +1,17 @@
 const MainFooter = Vue.component('main-footer', {
-    template: '#main-footer'
+    props: ['tip'],
+    template: '#main-footer',
+    computed: {
+        twitter_url: function () {
+            let text_content;
+            if (i18n.locale === 'pl') {
+                text_content = this.tip.text_content;
+            } else {
+                text_content = this.tip.text_content_en;
+            }
+
+            text_content = text_content + '&hashtags=SDG,HackathonGUS';
+            return encodeURI('https://twitter.com/intent/tweet?text=' + text_content);
+        }
+    }
 });
