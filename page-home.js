c57f65cc56f74402e5a41e1745c7e6a46feffa88
Krzysztof Antoniak <krzysztof@antoniak.in>
2018-11-10 08:54:56 +0100

    Add charts

--- page-home.js
+++ page-home.js
@@ -1,5 +1,34 @@
 const PageHome = Vue.component('page-home', {
     props: ['tip'],
+    data: function () {
+        return {
+            options: {
+                fullWidth: true,
+                chartPadding: {
+                    left: 0,
+                    right: 0
+                },
+                showArea: true,
+                low: 0,
+                onlyInteger: true,
+                axisX: {
+                    showGrid: false,
+                    showLabel: false,
+                    offset: 0
+                },
+                axisY: {
+                    showGrid: false,
+                    showLabel: false,
+                    offset: 0
+                },
+                plugins: [
+                    Chartist.plugins.ctPointLabels({
+                        textAnchor: 'middle'
+                    })
+                ]
+            }
+        };
+    },
     template: '#page-home',
     computed: {
         localized_text_content: function () {
@@ -7,6 +36,15 @@ const PageHome = Vue.component('page-home', {
                 return this.tip.text_content;
             } else {
                 return this.tip.text_content_en;
+            }
+        },
+        data: function () {
+            if (this.tip == null) {
+                return {};
+            }
+            return {
+                labels: this.tip.year_dataset,
+                series: [this.tip.dataset]
             }
         }
     }
---
+++ vue-chartist.js
@@ -0,0 +1,83 @@
+const PIE = 'Pie';
+const LINE = 'Line';
+const BAR = 'Bar';
+
+Vue.component('vue-chartist', {
+    template: '#vue-chartist',
+    props: {
+        type: {
+            type: String,
+            validator(val) {
+                return val === PIE || val === LINE || val === BAR;
+            },
+            default() {
+                return LINE;
+            }
+        },
+        data: {
+            type: Object,
+            required: true
+        },
+        options: {
+            type: Object
+        },
+        listener: {
+            type: Object
+        },
+        responsiveOptions: {
+            type: Array
+        }
+    },
+    data() {
+        return {
+            chartist: undefined
+        };
+    },
+    methods: {
+        updateEventListener(listener, type = 'on') {
+            listener = listener || {};
+            for (let event in listener) {
+                if (listener.hasOwnProperty(event)) {
+                    this.chartist[type](event, listener[event]);
+                }
+            }
+        },
+        renderChart() {
+            let data = this.data;
+            let options = this.options ? this.options : {};
+            let responsiveOptions = this.responsiveOptions ? this.responsiveOptions : [];
+            if (this.chartist) {
+                this.chartist.update(data, options, responsiveOptions);
+            } else {
+                this.chartist = new Chartist[this.type](this.$el, data, options, responsiveOptions);
+                this.updateEventListener(this.listener, 'on');
+            }
+        }
+    },
+    watch: {
+        data: {
+            handler: 'renderChart',
+            deep: true
+        },
+        options: {
+            handler: 'renderChart',
+            deep: true
+        },
+        responsiveOptions: {
+            handler: 'renderChart',
+            deep: true
+        },
+        listener(val, oldVal) {
+            this.updateEventListener(oldVal, 'off');
+            this.updateEventListener(val, 'on');
+        }
+    },
+    mounted() {
+        this.renderChart();
+    },
+    destroyed() {
+        if (this.chartist) {
+            this.chartist.detach();
+        }
+    }
+});
